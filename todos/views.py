7f811163dff259e705f1d88aeb32799d6f838243
Jordan Blakey <jordan@jordanblakey.com>
2017-01-29 16:28:34 -0600

    Finished.

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from .models import Todo
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello Worlds')
+    todos = Todo.objects.all()[:10]
+
+    context = {
+        'name': 'Jordan',
+        'todos': todos
+
+    }
+    return render(request, 'index.html', context)
+    # return HttpResponse('Hello Worlds')
+def details(request, id):
+    todo = Todo.objects.get(id=id)
+
+    context = {
+        'name': 'Jordan',
+        'todo':todo
+    }
+    return render(request, 'details.html', context)
+
+def add(request):
+
+    context = {
+        'name': 'Jordan'
+    }
+
+    if(request.method == 'POST'):
+        title = request.POST['title']
+        text = request.POST['text']
+
+        todo = Todo(title=title, text=text)
+        todo.save()
+
+        return redirect('/todos')
+    else:
+        return render(request, 'add.html', context)
